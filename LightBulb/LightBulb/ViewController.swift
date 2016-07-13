c0d7890f1eec188d68d7b12b32b061787f33f5b4
ellscha <escharlin@wesleyan.edu>
2016-07-13 17:09:54 -0400

    cool, did it my own way.

--- LightBulb/LightBulb/ViewController.swift
+++ LightBulb/LightBulb/ViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var lightBulb: UIView!
-
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lightBulb.backgroundColor = UIColor(red: CGFloat(255/255), green: CGFloat(63/255), blue: CGFloat(122/255), alpha: 1.0)
+
+        
         // TODO: Change background color of lightBulb view to red
     }
 
-    func changeColor(to color: UIColor) {
-        // TODO: Change background color to "color"
-    }
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
-        // TODO: Change background color when segmented control changes
+
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            lightBulb.backgroundColor = UIColor(red: CGFloat(255/255), green: CGFloat(63/255), blue: CGFloat(122/255), alpha: 1.0)
+        case 1:
+            lightBulb.backgroundColor = UIColor(red: CGFloat(255/255), green: CGFloat(255/255), blue: CGFloat(112/255), alpha: 1.0)
+        case 2:
+            lightBulb.backgroundColor = UIColor(red: CGFloat(90/255), green: CGFloat(90/255), blue: CGFloat(255/255), alpha: 1.0)
+        case 3:
+            lightBulb.backgroundColor = UIColor.greenColor()
+        default:
+            print("Not a color")
+                }
     }
 }
